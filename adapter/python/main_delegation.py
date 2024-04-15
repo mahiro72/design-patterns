9166c0f7de1670a0d500c3c763df965f4fcd1ab3
mahiro72 <mahiro.yoshie@gmail.com>
2024-04-16 01:38:02 +0900

    add: adapter, python

---
+++ adapter/python/main_delegation.py
@@ -0,0 +1,39 @@
+"""
+委譲(Delegation) パターン
+"""
+
+from abc import ABC, abstractmethod
+
+class Target(ABC):
+    @abstractmethod
+    def get_json(self) -> any: pass
+
+class Adaptee:
+    def __init__(self, xml_data: str | None = None) -> None:
+        self._xml_data = xml_data
+
+    def get_xml(self) -> any:
+        return self._xml_data
+
+class Adapter(Target, Adaptee):
+    def __init__(self, adaptee: Adaptee) -> None:
+        self._adaptee = adaptee
+
+    def get_json(self) -> any:
+        json_data = self.to_json_from_xml()
+        return json_data
+
+    def to_json_from_xml(self) -> any:
+        # なんらかの変換処理
+        return "json data"
+
+if __name__ == "__main__":
+    """
+    もう一つのファイル(main_inheritance.py)の委譲バージョン。
+    Adapterのインスタンス作成時にAdapteeをDIしている。
+    """
+
+    xml_getter = Adaptee(xml_data="xml data")
+    json_getter = Adapter(adaptee=xml_getter)
+    json_data = json_getter.get_json()
+    print(f"data: {json_data}")
---
+++ adapter/python/main_inheritance.py
@@ -0,0 +1,39 @@
+"""
+継承(Inheritance) パターン
+"""
+from abc import ABC, abstractmethod
+
+class Target(ABC):
+    @abstractmethod
+    def get_json(self) -> any: pass
+
+class Adaptee:
+    def __init__(self, xml_data: str | None = None) -> None:
+        self._xml_data = xml_data
+
+    def get_xml(self) -> any:
+        return self._xml_data
+
+class Adapter(Target, Adaptee):
+    def __init__(self, xml_data: str | None = None) -> None:
+        self._xml_data = xml_data
+
+    def get_json(self) -> any:
+        json_data = self.to_json_from_xml()
+        return json_data
+
+    def to_json_from_xml(self) -> any:
+        # なんらかの変換処理
+        return "json data"
+
+if __name__ == "__main__":
+    """
+    これまではTargetインターフェース満たす別のオブジェクトを利用してjsonデータを取得していた。
+    今回の例では、Targetインターフェースと他のクラス(Adaptee)を継承した、新しいクラス(Adapter)を用意することで、
+    xmlデータからjsonデータを取得できるようになり、既存のコードと置き換えができる。
+    元のインターフェースと他のクラスの両方を継承した、新しいクラスをラッパーとして活用するパターンをAdapterパターンという。
+    """
+
+    json_getter = Adapter(xml_data="xml data")
+    json_data = json_getter.get_json()
+    print(f"data: {json_data}")
