adca8a50c04f6fe63de6f4027da4a787e8555266
mahiro72 <mahiro.yoshie@gmail.com>
2024-04-16 03:32:29 +0900

    add: template method, python

---
+++ template-method/python/main.py
@@ -0,0 +1,29 @@
+from abc import ABC, abstractmethod
+
+class AbstractDisplay(ABC):
+    @abstractmethod
+    def load_data(self) -> any: pass
+
+    def display(self) -> None:
+        data = self.load_data()
+        print("data loaded")
+        print(data)
+
+class CSVDisplay(AbstractDisplay):
+    def __init__(self, csv_path: str | None = None) -> None:
+        self._csv_path = csv_path
+
+    def load_data(self) -> any:
+        # csv_pathからcsvを読み込む処理。今回はサンプル実装のため割愛
+        return "csv data"
+
+if __name__ == "__main__":
+    """
+    抽象クラスであるスーパークラス(AbstractDisplay) に、テンプレート機能と抽象メソッドがある。
+    テンプレート機能内で、抽象メソッドが利用される。
+    サブクラスでは、抽象メソッドの実装が行われ、今回の例ではcsvをロードするデータが実装されている。
+    ExcelDisplayなど別データをロードするクラスにも機能拡張することができる。
+    """
+
+    csv_display = CSVDisplay(csv_path="./xxx/xxx.csv")
+    csv_display.display()
